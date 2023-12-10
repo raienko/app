910f1c213111bd759b0d2828b49a43b11dfbf8dd
raienkoa <raienko.a@gmail.com>
2023-12-10 21:18:35 +0200

    Basic setup for absolute imports

--- App.tsx
+++ App.tsx
@@ -1,13 +1,6 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- */
-
 import React from 'react';
 import type {PropsWithChildren} from 'react';
-import {env} from './src/constants';
+import {env} from 'src/constants';
 import {
   SafeAreaView,
   ScrollView,
