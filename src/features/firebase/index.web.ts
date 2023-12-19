b64585c16c6a93a670dc5666eb1f88c9010978d9
raienkoa <raienko.a@gmail.com>
2023-12-20 00:44:21 +0200

    Temporary workaround for firebase

--- src/features/firebase/index.web.ts
+++ src/features/firebase/index.web.ts
@@ -1,22 +1,9 @@
-import Config from 'react-native-config';
-import * as Firebase from 'firebase/app';
-import * as RemoteConfig from 'firebase/remote-config';
+import RemoteConfig from 'firebase/remote-config';
+import {app} from './config.web.ts';
 
-const config = {
-  apiKey: `${Config.FIREBASE_API_KEY}`,
-  projectId: `${Config.FIREBASE_PROJECT_ID}`,
-  databaseURL: `${Config.FIREBASE_DB_URL}`,
-  authDomain: `${Config.FIREBASE_AUTH_DOMAIN}`,
-  // OPTIONAL
-  storageBucket: `${Config.FIREBASE_STORAGE_BUCKET}`,
-  messagingSenderId: `${Config.FIREBASE_MESSAGING_SENDER_ID}`,
-};
+// const remoteConfig = RemoteConfig.getRemoteConfig(app);
 
-const app = Firebase.initializeApp(config);
-const remoteConfig = RemoteConfig.getRemoteConfig(app);
-
-export const fetchRemoteConfig = async (defaultConfig: any) => {
-  remoteConfig.defaultConfig = defaultConfig;
-  await RemoteConfig.fetchAndActivate(remoteConfig);
-  return RemoteConfig.getAll(remoteConfig);
+export const fetchRemoteConfig = async () => {
+  // await RemoteConfig.fetchAndActivate(remoteConfig);
+  // return RemoteConfig.getAll(remoteConfig);
 };
