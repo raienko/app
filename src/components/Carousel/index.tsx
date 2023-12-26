c856ed37dd6b4d6f650b561ab5741b087e411609
raienkoa <raienko.a@gmail.com>
2023-12-26 16:48:38 +0200

    Save current state

--- src/components/Carousel/index.tsx
+++ src/components/Carousel/index.tsx
@@ -2,14 +2,27 @@ import React from 'react';
 import RNCarousel from 'react-native-reanimated-carousel';
 import {rem} from '~/src/utils';
 import Text from '~/src/components/Text';
-import {Button, View} from 'react-native';
+import {Button, StyleSheet, View} from 'react-native';
 import {sizes} from '~/src/constants';
 
-type CarouselProps = any;
+type CarouselProps = {
+  width: number;
+  height: number;
+};
 
-export default function Carousel(props: CarouselProps) {
+export default function Carousel({
+  width = rem(100),
+  height = rem(100),
+  ...rest
+}: CarouselProps) {
   const data = [...new Array(6).keys()];
   const ref = React.useRef<typeof RNCarousel>(null);
+
+  const renderSlide = ({index}) => (
+    <View style={styles.slide}>
+      <Text style={{textAlign: 'center'}} value={`SLIDE_${index}`} />
+    </View>
+  );
 
   return (
     <>
@@ -18,28 +31,27 @@ export default function Carousel(props: CarouselProps) {
       <RNCarousel
         ref={ref}
         loop
-        autoPlay={true}
+        autoPlay
         data={data}
-        style={{
-          borderWidth: 1,
-          borderRadius: sizes.uiRadius,
-        }}
-        width={rem(100)}
-        height={rem(100)}
+        width={width}
+        height={height}
+        style={styles.wrapper}
+        renderItem={renderSlide}
         scrollAnimationDuration={1000}
-        renderItem={({index}) => (
-          <View
-            style={{
-              width: rem(100),
-              height: rem(100),
-              alignItems: 'center',
-              justifyContent: 'center',
-            }}>
-            <Text style={{textAlign: 'center'}} value={`SLIDE_${index}`} />
-          </View>
-        )}
-        {...props}
+        {...rest}
       />
     </>
   );
 }
+
+const styles = StyleSheet.create({
+  wrapper: {
+    borderWidth: 1,
+    borderRadius: sizes.uiRadius,
+  },
+  slide: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
