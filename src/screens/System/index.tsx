d94e0f765fa053e47bab0f079c60be52fe081b84
raienkoa <raienko.a@gmail.com>
2024-01-01 23:55:01 +0200

    Cleanup

--- src/screens/System/index.tsx
+++ src/screens/System/index.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import {StyleSheet, View, Button} from 'react-native';
+import {StyleSheet, View} from 'react-native';
 import {system, firebase} from '~/src/features';
 import {StatusBar} from '~/src/components';
 import {useReloadOnWindowChange, logger} from '~/src/utils';
@@ -10,42 +10,12 @@ export default function System(): React.JSX.Element {
   system.useRemoteConfigSetup();
   system.usePermissionsCheck();
   useReloadOnWindowChange();
-
-  const firestore = firebase.useFirestore('test', (data: any) =>
-    logger.info('Firestore:', data),
-  );
-
-  const database = firebase.useDatabase('test', (data: any) =>
-    logger.info('FireDatabase:', data),
-  );
+  firebase.useFirestore('test', (data: any) => logger.info('Firestore:', data));
+  firebase.useDatabase('test', (data: any) => logger.info('Database:', data));
 
   return (
     <View style={styles.wrapper}>
       <StatusBar />
-      <Button
-        title="Firestore write 1"
-        onPress={() => firestore.write('3g3T0XlesUUcp1PRPD0w', {test: 1})}
-      />
-      <Button
-        title="Firestore write 2"
-        onPress={() => firestore.write('3g3T0XlesUUcp1PRPD0w', {test: 2})}
-      />
-      <Button
-        title="Firestore create X"
-        onPress={() => firestore.create({id: 'x', test: 2})}
-      />
-      <Button
-        title="Firestore remove X"
-        onPress={() => firestore.remove('x')}
-      />
-      <Button
-        title="Database write 1"
-        onPress={() => database.write('test', 1)}
-      />
-      <Button
-        title="Database write 2"
-        onPress={() => database.write('test', 2)}
-      />
     </View>
   );
 }
@@ -53,8 +23,6 @@ export default function System(): React.JSX.Element {
 const styles = StyleSheet.create({
   wrapper: {
     ...StyleSheet.absoluteFillObject,
-    // pointerEvents: 'box-none',
-    backgroundColor: 'white',
-    paddingTop: 100,
+    pointerEvents: 'box-none',
   },
 });
