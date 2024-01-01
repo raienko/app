950228665fd5c3e6b2bc0d2aebb20e0165c66d8b
raienkoa <raienko.a@gmail.com>
2024-01-01 17:06:08 +0200

    Permissions basic preparation

--- src/features/system/permissions.ts
+++ src/features/system/permissions.ts
@@ -2,13 +2,24 @@ import {useEffect} from 'react';
 import {AppState} from 'react-native';
 import slice from './slice';
 import store, {useStoreSelector} from '~/src/features/store';
+import Permissions, {Permission} from 'react-native-permissions';
+import {isIOS, isWeb} from '~/src/utils';
 
 export type PermissionTypes = 'camera' | 'location' | 'notifications';
+
+const permissions: {[key in PermissionTypes]: Permission} = {
+  camera: isIOS
+    ? Permissions?.PERMISSIONS.IOS.CAMERA
+    : Permissions?.PERMISSIONS.ANDROID.CAMERA,
+  location: Permissions?.PERMISSIONS.IOS.LOCATION_WHEN_IN_USE,
+  notifications: 'notifications',
+};
 
 const checkPermissions = () => {
   if (AppState.currentState !== 'active') {
     return;
   }
+  checkCameraPermission();
 };
 
 export const setPermission = (type: PermissionTypes, permission: string) =>
@@ -25,8 +36,34 @@ export const usePermission = (type: any) =>
 export const usePermissionsCheck = () => {
   useEffect(() => {
     const listener = AppState.addEventListener('change', checkPermissions);
+    checkPermissions();
     return () => {
       listener.remove();
     };
   }, []);
+};
+
+const checkPermission = (permission: any) => {
+  if (isWeb) {
+    // @ts-ignore
+    return navigator.permissions
+      .query({name: permission})
+      .then(result => result.state);
+  }
+
+  return Permissions.check(permission);
+};
+
+export const checkCameraPermission = async () => {
+  console.log('Permissions check...');
+  if (isWeb) {
+    // @ts-ignore
+    return navigator.permissions
+      .query({name: 'camera'})
+      .then((result: any) => result.state)
+      .then((perm: any) => console.log('Web Camera: ', perm));
+  }
+  return Permissions.check(permissions.camera).then(permission =>
+    console.log('CAMERA: ', permission),
+  );
 };
