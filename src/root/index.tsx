179e3e2a1648de63ecb81618af10ce5a2f812e25
raienkoa <raienko.a@gmail.com>
2024-01-02 21:20:07 +0200

    Cleanup

--- src/root/index.tsx
+++ src/root/index.tsx
@@ -2,7 +2,6 @@ import React from 'react';
 import Router from '~/src/features/navigation/Router';
 import StoreProvider from '~/src/features/store/StoreProvider';
 import {GestureHandlerRootView} from 'react-native-gesture-handler';
-import {BottomSheetModalProvider} from '@gorhom/bottom-sheet';
 import {StyleSheet} from 'react-native';
 import {LaunchScreen} from '~/src/screens';
 
@@ -11,9 +10,7 @@ export default function App(): React.JSX.Element {
     <>
       <GestureHandlerRootView style={styles.wrapper}>
         <StoreProvider>
-          <BottomSheetModalProvider>
-            <Router />
-          </BottomSheetModalProvider>
+          <Router />
         </StoreProvider>
       </GestureHandlerRootView>
       {/* @ts-ignore */}
